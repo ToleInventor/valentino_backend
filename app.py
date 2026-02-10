3253480388fefe6af26c2d9d1ee0e72e871b95ca
ToleInventor <168644336+ToleInventor@users.noreply.github.com>
2026-02-11 02:24:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'caxtonetechnologieske@gmail.com'
 app.config['MAIL_PASSWORD'] = 'itdh gorw ixcm pkdv'
 
@@ -82,6 +82,7 @@ def Test():
 
 if __name__ == '__main__':
    app.run(debug=True)
+
 
 
 
